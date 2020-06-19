cc1581990c34bce74d27d8e475c4dc765a96a105
fstrollo <fstrollo@rtl.be>
2020-06-19 12:22:44 +0200

    Add Unit Tests

--- Interparking.FileReader/Interparking.FileReader.Test/TextReaderTest.cs
+++ Interparking.FileReader/Interparking.FileReader.Test/TextReaderTest.cs
@@ -1,6 +1,7 @@
 using Interparking.FileReader.Interfaces;
 using Interparking.FileReader.Readers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 
 namespace Interparking.FileReader.Test
@@ -11,6 +12,7 @@ namespace Interparking.FileReader.Test
         #region Variables Declaration
 
         private IReader reader;
+        private readonly string contentText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit scelerisque in dictum non consectetur a erat. Sit amet justo donec enim diam vulputate. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Eget gravida cum sociis natoque penatibus et magnis dis. Habitant morbi tristique senectus et netus et. Interdum consectetur libero id faucibus nisl tincidunt eget nullam. Aliquam purus sit amet luctus. Fringilla ut morbi tincidunt augue interdum velit. Neque sodales ut etiam sit. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Ornare suspendisse sed nisi lacus sed. Iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui. Quam nulla porttitor massa id neque aliquam vestibulum morbi. Dignissim diam quis enim lobortis scelerisque fermentum dui faucibus. Turpis egestas integer eget aliquet.";
 
         #endregion
 
@@ -19,7 +21,9 @@ namespace Interparking.FileReader.Test
         [TestInitialize]
         public void TestInitialize()
         {
-            reader = new TextReader();
+            Mock<IEncryption> encryctionMock = new Mock<IEncryption>();
+            encryctionMock.Setup(x => x.Decrypt(It.IsAny<string>())).Returns((string x) => Reverse(x));
+            reader = new TextReader(encryctionMock.Object);
         }
 
         #endregion
@@ -45,28 +49,31 @@ namespace Interparking.FileReader.Test
         {
             string text = reader.Read(".\\ReadFile.txt");
             Assert.IsNotNull(text);
+            Assert.IsTrue(string.Equals(text, contentText));
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void ReadXml_PathNull_Test()
+        public void ReadFile_Encrypted_Test()
         {
-            reader.ReadXmlFile(null);
+            string text = reader.Read(".\\ReadFileReversed.txt", true);
+            Assert.IsNotNull(text);
+            Assert.IsTrue(string.Equals(text, contentText));
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void ReadXml_PathEmpty_Test()
+        #endregion
+
+        #region Business
+
+        #region Priavete
+
+        private string Reverse(string text)
         {
-            reader.ReadXmlFile("   ");
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
 
-        [TestMethod]
-        public void ReadXml_Test()
-        {
-            string xml = reader.ReadXmlFile(".\\ReadXml.xml");
-            Assert.IsNotNull(xml);
-        }
+        #endregion
 
         #endregion
     }
